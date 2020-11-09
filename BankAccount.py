2b3c43a728906b118c8eda6383d0442a4e52c185
Hani Jandali <hanijandali@Hanis-MacBook-Pro.local>
2020-11-09 00:24:09 -0800

    This is the initial class BankAccount with early defined functions and methods

--- BankAccount.py
+++ BankAccount.py
@@ -1,5 +1,12 @@
-print("Hello World")
 
-print("testing")
+#First is initializing class
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+        #balance is zero, will add deposits to increase balance
 
-1 + 1
+    def deposit (self, amount) 
+        #this function will take the amount put in as a deposit and will
+        #add it to the current balance 
+        #This is why we require both the self and amount parameters
+        self.balance = self.balance + amount
