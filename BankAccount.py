561877b8e2b4f549800ec0244a1d30689ddf800b
Hani Jandali <hanijandali@Hanis-MacBook-Pro.local>
2020-11-09 16:45:32 -0800

    This includes the attributes under the Bank Account class

--- BankAccount.py
+++ BankAccount.py
@@ -1,8 +1,11 @@
 
 #First is initializing class
 class BankAccount:
-    def __init__(self):
+    def __init__(self, full_name, account_number, routing_number, balance):
         self.balance = 0
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.full_name = full_name
         #balance is zero, will add deposits to increase balance
 
     def deposit (self, amount) 
@@ -10,3 +13,14 @@ class BankAccount:
         #add it to the current balance 
         #This is why we require both the self and amount parameters
         self.balance = self.balance + amount
+
+    def withdraw(self, amount):
+        #This function will take the amount withdrawn and subtract from the balance
+        if self.balance >= amount:
+            self.balance = self.balance - amount
+        else:
+            self.balance - 10.00
+            print("Insufficient funds")
+        
+    def print_balance(self):
+        return self.balance
